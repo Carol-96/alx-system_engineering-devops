4ff68ab90dce7b9e9754d6974272328502e77b19
Carol-96 <karolynkalumu@gmail.com>
2024-06-18 13:20:52 -0700

    top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+    this module contains the function top_ten
+'''
+import requests
+from sys import argv
+
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data and 'children' in data['data']:
+            for post in data['data']['children']:
+                print(post['data']['title'])
+        else:
+            print(None)
+    else:
+        print(None)
